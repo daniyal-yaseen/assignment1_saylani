a49c568fb81af1593ce6eb3579d4c2003562531f
daniyal-yaseen <danimemon77@gmail.com>
2019-11-05 19:56:01 +0500

    assignment1

---
+++ q3.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+# datetime object containing current date and time
+t = datetime.now()
+ 
+print("Current date and time is :", t)
---
+++ q5.py
@@ -0,0 +1,3 @@
+name = input("Enter First name and Last name: ")
+first_name, last_name = name.split(' ')
+print(last_name,first_name) 
